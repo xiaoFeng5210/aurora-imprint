7a036b0c00302ecdad05d5ca171a9c653d695cb7
Eadweard <804875728@qq.com>
2024-03-25 21:52:43 +0800

    finish

---
+++ web/src/components/nav/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Nav() {
+	return (
+    <div className="tw-w-full tw-p-4 tw-h-[45px] tw-bg-amber-100">
+      
+    </div>
+  )
+}
+
+export default Nav
--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -3,6 +3,7 @@ import SlideBar from '@/components/slide-bar'
 import { Outlet } from 'react-router-dom'
 import { motion } from 'framer-motion'
 import { useNavigate } from 'react-router-dom'
+import Nav from '@/components/nav/Navbar'
 
 const Home: FC = () => {
   const navigate = useNavigate()
@@ -14,6 +15,7 @@ const Home: FC = () => {
     <div className='tw-w-screen tw-h-dvh tw-overflow-hidden tw-flex tw-flex-row'>
       <SlideBar />
       <main className="tw-flex-1">
+        <Nav />
         <motion.div layout className='tw-p-4 tw-w-full tw-h-full'>
           <Outlet />
         </motion.div>
