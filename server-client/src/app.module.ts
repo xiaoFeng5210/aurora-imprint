df7c81e25af8018f66e54113e2c18471022aefa7
Eadweard <804875728@qq.com>
2024-03-31 21:12:55 +0800

    prisma初始化

--- server-client/src/app.module.ts
+++ server-client/src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, ConfigModule.forRoot(), PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
