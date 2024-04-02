942c6744ab35cac367f611b6784588b97ccff9ff
Eadweard <804875728@qq.com>
2024-04-02 10:07:00 +0800

    update

--- server-client/src/article/article.service.ts
+++ server-client/src/article/article.service.ts
@@ -10,19 +10,19 @@ export class ArticleService {
   create(createArticleDto: CreateArticleDto) {
     return 'This action adds a new article';
   }
-
+  
   findAll() {
     return this.prisma.article.findMany()
   }
-
+  
   findOne(id: number) {
     return `This action returns a #${id} article`;
   }
-
+  
   update(id: number, updateArticleDto: UpdateArticleDto) {
     return `This action updates a #${id} article`;
   }
-
+  
   remove(id: number) {
     return `This action removes a #${id} article`;
   }
