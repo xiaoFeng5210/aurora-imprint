f9b331e58f1b7cf686ac68e66df7398564092d41
Eadweard <804875728@qq.com>
2024-04-02 11:29:15 +0800

    修复prisma报错的问题

--- server-client/src/article/article.module.ts
+++ server-client/src/article/article.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { ArticleService } from './article.service';
 import { ArticleController } from './article.controller';
 import {PrismaModule} from "../core/prisma/prisma.module";
+import {PrismaService} from "../core/prisma/prisma.service";
 
 @Module({
   controllers: [ArticleController],
-  providers: [ArticleService],
+  providers: [ArticleService, PrismaService],
   imports: [PrismaModule]
 })
 export class ArticleModule {}
--- server-client/src/user/user.module.ts
+++ server-client/src/user/user.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { PrismaModule } from '../core/prisma/prisma.module';
+import {PrismaService} from "../core/prisma/prisma.service";
 
 @Module({
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService, PrismaService],
+  imports: [PrismaModule],
 })
 export class UserModule {}
