9cd60f16357c4bedcef8f413a98701c2900216e6
Eadweard <aurora.coder.zqf@gmail.com>
2024-04-24 11:09:39 +0800

    登陆接口加上login中间件

--- server-client/src/user/user.module.ts
+++ server-client/src/user/user.module.ts
@@ -1,12 +1,19 @@
-import { Module } from '@nestjs/common';
+import {MiddlewareConsumer, Module, NestModule} from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { PrismaModule } from '../core/prisma/prisma.module';
 import {PrismaService} from "../core/prisma/prisma.service";
+import {LoginMiddleware} from "../common/middleware/login.middleware";
 
 @Module({
   controllers: [UserController],
   providers: [UserService, PrismaService],
   imports: [PrismaModule],
 })
-export class UserModule {}
+export class UserModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoginMiddleware)
+      .forRoutes('users/login');
+  }
+}
