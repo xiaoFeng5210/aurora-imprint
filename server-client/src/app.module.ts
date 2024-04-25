383c52014d9473c7082e48711f9da488c8deb51e
Eadweard <aurora.coder.zqf@gmail.com>
2024-04-25 10:50:32 +0800

    auth init

--- server-client/src/app.module.ts
+++ server-client/src/app.module.ts
@@ -5,9 +5,10 @@ import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './core/prisma/prisma.module';
 import { ArticleModule } from './article/article.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UserModule, PrismaModule, ArticleModule],
+  imports: [UserModule, PrismaModule, ArticleModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
