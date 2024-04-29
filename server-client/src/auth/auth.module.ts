5559e99312c60735f35459cdad990991f01090ef
Eadweard <aurora.coder.zqf@gmail.com>
2024-04-29 11:28:29 +0800

    jwt service

--- server-client/src/auth/auth.module.ts
+++ server-client/src/auth/auth.module.ts
@@ -4,10 +4,12 @@ import { AuthService } from './auth.service';
 import {UserModule} from "../user/user.module";
 import {UserService} from "../user/user.service";
 import {PrismaService} from "../core/prisma/prisma.service";
+import {JwtModule} from "@nestjs/jwt";
+import {JwtService} from "@nestjs/jwt";
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService, UserService, PrismaService],
-  imports: [UserModule],
+  providers: [AuthService, UserService, PrismaService, JwtService],
+  imports: [UserModule, JwtModule],
 })
 export class AuthModule {}
--- server-client/src/auth/auth.service.ts
+++ server-client/src/auth/auth.service.ts
@@ -1,18 +1,24 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserService } from '../user/user.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-	constructor(private userService: UserService) {}
+	constructor(private userService: UserService, private jwtService: JwtService) {}
 
-	async signIn(username: string, pass: string): Promise<any> {
-		const user = await this.userService.findOne(username);
+	async signIn(email: string, pass: string): Promise<{access_token: string}> {
+		const user = await this.userService.findOne(email);
 		if (user?.password !== pass) {
 			throw new UnauthorizedException();
 		}
 		const { password, ...result } = user;
 		// TODO: Generate a JWT and return it here
-		// instead of the user object
-		return result;
+		
+		const payload = { sub: user.uid, username: user.email };
+		return {
+			access_token: await this.jwtService.signAsync(payload),
+		};
 	}
+	
+	
 }
